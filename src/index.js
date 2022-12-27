9e66a47c901ee8e9ad1e64ad38e9e7ee7ca74978
jaredhuffer1 <jaredhuffer1@gmail.com>
2022-12-27 17:30:23 -0500

    WIP

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './less/index.less'
 
-// Your code goes here!
+// 1-loud
+window.onload = function (evt) {
+    console.log(`event ${evt.type} Fried! READY TO GO!!`)
+const heading = document.querySelector('h1');
+heading.textContent = "AquaAire AutoBus";
+// 2-copy
+window.addEventListener('copy', () => {
+navigator.clipboard.readText()
+.then(text => {
+    heading.textContent += text
+})
+})
+}
