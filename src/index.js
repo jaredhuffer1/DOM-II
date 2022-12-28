795e625dd83641ac3aab5839123b71b07419a09a
jaredhuffer1 <jaredhuffer1@gmail.com>
2022-12-28 08:28:58 -0500

    good to go

--- src/index.js
+++ src/index.js
@@ -12,4 +12,38 @@ navigator.clipboard.readText()
     heading.textContent += text
 })
 })
+
+// 3 - click
+document.body,addEventListener('click', evt => {
+evt.target.classList.toggle('mirror')
+})
+
+// 4-dblclick 
+document.body.addEventListener('dblclick', evt => {
+    evt.target.outerHTML = '';
+})
+// 5-keydown
+window.addEventListener('keydown', evt => {
+   if (evt.key == 6) {
+    document.body.innerHTML = '<h1>You are on the AquaAire AutoBus</h1>';
+   }
+})
+// 6-mousemove
+document.body.addEventListener('mousemove', evt => {
+    const { clientX, clientY } = evt
+  //  console.log(`mouse is at ${clientX} and now its at ${clientY}`);
+})
+// 7-mouseenter
+// 8-mouseleave
+const destinations = document.querySelectorAll('.destination');
+for (let destination of destinations) {
+destination.addEventListener("mouseenter", () => {
+   destination.style.fontWeight = 'bold';
+})
+destination.addEventListener('mouseleave', () => {
+    setTimeout(() => {
+        destination.style.fontWeight = 'initial';
+    },400)
+})
+}
 }
